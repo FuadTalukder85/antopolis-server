62584bf015a75e026558719cbfd84781d5457019
FuadTalukder85 <webfuadtk@gmail.com>
2025-06-28 00:01:27 +0600

    use multer for save image

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 4900;
@@ -24,7 +25,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 async function run() {
   try {
     await client.connect();
@@ -32,13 +34,20 @@ async function run() {
 
     const db = client.db("antopolis");
     const foodItemCollection = db.collection("foodItem");
+    const categoryCollection = db.collection("categoryItems");
 
     // POST: Add a new food item
-    app.post("/foodItem", async (req, res) => {
-      const foodItem = req.body;
+    app.post("/foodItem", upload.single("image"), async (req, res) => {
+      const { foodName, category } = req.body;
+      const imageFile = req.file;
 
       try {
-        const result = await foodItemCollection.insertOne(foodItem);
+        const result = await foodItemCollection.insertOne({
+          foodName,
+          category,
+          imagePath: imageFile?.filename,
+        });
+
         res.status(201).json({ success: true, data: result });
       } catch (error) {
         console.error("Insert error:", error);
@@ -47,7 +56,8 @@ async function run() {
           .json({ success: false, message: "Internal server error" });
       }
     });
-
+    // Serve images from /uploads
+    app.use("/uploads", express.static(path.join(__dirname, "uploads")));
     // GET: All food items
     app.get("/foodItem", async (req, res) => {
       try {
@@ -96,7 +106,36 @@ async function run() {
         res.status(400).json({ message: "Invalid ID format" });
       }
     });
+    // POST: Add a new Category
+    app.post("/category", async (req, res) => {
+      const { category } = req.body;
 
+      const existing = await categoryCollection.findOne({ category });
+      if (existing) {
+        return res.status(409).json({ message: "Category already exists" });
+      }
+
+      try {
+        const result = await categoryCollection.insertOne({ category });
+        res.status(201).json({ success: true, data: result });
+      } catch (error) {
+        console.error("Insert error:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal server error" });
+      }
+    });
+
+    // GET: All category items
+    app.get("/category", async (req, res) => {
+      try {
+        const result = await categoryCollection.find().toArray();
+        res.status(200).json(result);
+      } catch (error) {
+        console.error("Fetch all error:", error);
+        res.status(500).json({ message: "Failed to fetch food items" });
+      }
+    });
     // Health check
     app.get("/", (req, res) => {
       res.json({
